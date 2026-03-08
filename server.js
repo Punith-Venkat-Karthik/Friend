9be1a9a32c6e4773000fde8ff237658986e84254
Punith-Venkat-Karthik <golipunith@gmail.com>
2026-03-08 06:33:37 +0530

    Rename html to index.html and fix css loading

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "FriendshipBloom.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
